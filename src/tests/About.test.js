835a8672e46db12c163dd660f6feebe32e9e660d
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-08-25 21:20:29 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('About test', () => {
+  test('página possui texto "This application"', () => {
+    renderWithRouter(<About />);
+
+    const pokedexInfo = screen.getByText(/this application/i);
+    expect(pokedexInfo).toBeInTheDocument();
+  });
+
+  test('página possui texto "About Pokedéx"', () => {
+    renderWithRouter(<About />);
+
+    const aboutPokedex = screen.getByRole('heading', {
+      level: 2,
+      name: /about pokédex/i,
+    });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  test('página possui imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const pokedexImg = screen.getByRole('img');
+    expect(pokedexImg.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(pokedexImg).toBeInTheDocument();
+  });
+});
