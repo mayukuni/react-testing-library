9979c4e26015822961e521eba4b30e22a8378f65
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-08-26 10:17:15 -0300

    parte do requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+// import pokemons from '../data';
+// import { Pokedex } from '../components';
+
+describe('Pokedex test', () => {
+  test('página possui texto "Encountered pokémons"', () => {
+    renderWithRouter(<App />);
+
+    const pokedexText = screen.getByRole('heading', {
+      level: 2,
+      name: /encountered pokémons/i,
+    });
+    expect(pokedexText).toBeInTheDocument();
+  });
+
+  test('página possui botão e mostra o próximo pokémon quando clicado', () => {
+    renderWithRouter(<App />);
+
+    const pokedexButton = screen.getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    expect(pokedexButton).toBeInTheDocument();
+  });
+});
