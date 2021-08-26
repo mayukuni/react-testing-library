cc8f89735f1fa2d2a77f1b6fb125a4e389f031b7
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-08-25 21:46:22 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('NotFound test', () => {
+  test('página contém o text "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundText = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  test('página possui uma imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundImg = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(notFoundImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(notFoundImg).toBeInTheDocument();
+  });
+});
