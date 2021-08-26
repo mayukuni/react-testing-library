fb7cca154230aa53e1a02b5dba96c3bfad560331
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-08-25 21:33:27 -0300

    parte do requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('FavoritePokemons test', () => {
+  test('página possui texto "No favorite pokemon found" se não tiver favoritos', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+
+    const favoriteNotFound = screen.getByText('No favorite pokemon found');
+    expect(favoriteNotFound).toBeInTheDocument();
+  });
+
+//  continuar depois
+});
